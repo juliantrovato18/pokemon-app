a1f597279c6c04bd15fd7e538bdb812b2ee73460
juliantrovato <julian_todobien@hotmail.com>
2022-07-27 20:55:43 -0300

    pokemon app in process

---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+export const Card = ()=>{
+    return (
+        <>
+            <div className="card">
+                <h2>Charmander</h2>
+                <img className="img" src="../../img/char.svg" alt="" />
+            </div>
+        </>
+    )
+}
---
+++ src/Poke-info.jsx
@@ -0,0 +1,25 @@
+export const PokeInfo = ()=>{
+    return (
+        <>
+            <div>
+                <h3>charmander</h3>
+                <img  src="../img/char.svg" alt="" />
+                <div className="abilities">
+                    <div className="group">
+                        <h3>blaze</h3>
+                    </div>
+                    <div className="group">
+                        <h3>Solar</h3>
+                    </div>
+                </div>
+                <div className="base-state">
+                        <h3>HP:30</h3>
+                        <h3>attack:30</h3>
+                        <h3>speed:30</h3>
+                        <h3>life:30</h3>
+                        <h3>defense:30</h3>
+                </div>    
+            </div>
+        </>
+    )
+}
